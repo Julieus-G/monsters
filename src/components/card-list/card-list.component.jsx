4f5cea2023b5589545610bd87588c6d2e58a2329
Julieus-G <goodwonder5@gmail.com>
2022-03-07 14:46:42 +0300

    initial commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        return <div>This is a cardlist component</div>
+    }
+}
+
+export default CardList;
