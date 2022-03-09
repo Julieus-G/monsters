8fb2897ee45a0f9a89752efa89a37f9b60ad8d4a
Julieus-G <goodwonder5@gmail.com>
2022-03-09 14:33:24 +0300

    addind css

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,16 +2,16 @@ import { Component } from "react";
 import "./search-box.styles.css";
 
 class SearchBox extends Component {
-        render() {
-                return (
-                        <input
-                                type="search"
-                                className={`search-box ${this.props.className}`}
-                                placeholder={this.props.placeholder}
-                                onChange={this.props.onChangeHandler}
-                        />
-                );
-        }
+  render() {
+    return (
+      <input
+        type="search"
+        className={`search-box ${this.props.className}`}
+        placeholder={this.props.placeholder}
+        onChange={this.props.onChangeHandler}
+      />
+    );
+  }
 }
 
 export default SearchBox;
